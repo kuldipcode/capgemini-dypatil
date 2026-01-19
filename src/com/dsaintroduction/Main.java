58df63ac3e7ed5cd57787c321cc6b1ce30f7d580
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2026-01-19 12:12:53 +0530

    loop

--- src/com/dsaintroduction/Main.java
+++ src/com/dsaintroduction/Main.java
@@ -1,10 +1,20 @@
 package com.dsaintroduction;
 
 public class Main {
+    public static void main(String[] args) {
+        int prev2 = 0;
+        int prev1 = 1;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println(prev2);
+        System.out.println(prev1);
 
-	}
-
+        for(int fibo = 0; fibo < 18; fibo++) {
+            int newFibo = prev1 + prev2;
+            System.out.println(newFibo);
+            prev2 = prev1;
+            prev1 = newFibo;
+        }
+    }
 }
+
+//Java
